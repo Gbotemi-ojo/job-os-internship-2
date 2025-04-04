71fc9c1fc98b423b4ab53f3b64ad061e8ff9cc7c
gbotemi <hommzmum@gmail.com>
2025-04-04 20:11:00 +0100

    updated database url

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,12 +7,13 @@ export default defineConfig({
   dialect: 'postgresql',
   // driver: "pg",
   dbCredentials: {
-    host: process.env.DB_HOST as string,
-    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
-    database: process.env.DB_NAME as string,
-    user: process.env.DB_USER as string,
-    password: process.env.DB_PASS as string,
-    ssl : process.env.DB_SSL=="true"?true:false,
+    // host: process.env.DB_HOST as string,
+    // port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
+    // database: process.env.DB_NAME as string,
+    // user: process.env.DB_USER as string,
+    // password: process.env.DB_PASS as string,
+    // ssl : process.env.DB_SSL=="true"?true:false,
+   url : process.env.DB_URL as string
   },
   verbose:true,
   strict:true
